0be05a4dd97dd9ef40a2639c2058d23e89e09e97
Ebenezer <ETSIWAH@GMAIL.COM>
2020-10-08 14:26:45 +0000

    Fetch API to load questions from local JSON file in Javascrit

---
+++ 10.Fetch API to load questions from local JSON file/game.js
@@ -0,0 +1,92 @@
+const question = getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarfull = document.getElementById("progressBarfull");
+
+let currentQuestion={}
+let acceptingAnswers =false;
+let score =0;
+let questioncounter=0;
+let availableQuestions =[];
+
+let questions = [];
+fetch('questions.json')
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+//constants
+const CORRECT_BONUS=10;
+const MAX_QUESTIONS=3;
+
+startGame =() =>{
+	questioncounter =0;
+	score =0;
+	availableQuestions=[...questions];
+	getNewQuestion();
+};
+
+getNewQuestion = () =>{
+	if(availableQuestions.length === 0 || questioncounter >=MAX_QUESTIONS){
+		localStorage.setItem('mostRecentScore', score);
+	
+	
+		retun window.location.assign("/end.html");
+	}
+	questioncounter++;
+	progressText.innerText = `Question${questionCounter}/${MAX_QUESTIONS}`;
+	//update the progress bar of the quiz app
+	progressBarfull.style.width = '([questionCounter/MAX_QUESTIONS)*100]px';
+
+	const questionIndex =Math.floor(Math.random()*availableQuestions.length);
+	currentQuestion =availableQuestions[questionIndex];
+	question.innerText = currentQuestion.question;
+
+	choices.forEach(choice =>{
+
+		const number =choice.dataset["number"];
+		choice.innerText =currentQuestion["choice" +number];
+});
+
+availableQuestions.splice(questionIndex,1);
+acceptingAnswers =true;
+};
+
+choices.forEach((choice =>{
+choice.AddEventListener("click", e => {
+	if(!acceptingAnswers) return;
+
+	acceptingAnswers = false;
+	const selectedChoice = e.target;
+	const selectedAnswer =selectedChoice.dataset["number"];
+
+	const classToApply =
+	selectedAnswer ==currentQuestion.answer?"correct" :" incorrect";
+
+	if(classToApply ==="correct"){
+		incrementScore(CORRECT_BONUS);
+	}	
+    
+   selectedChoice.parentElement.classList.add(classToApply);
+
+   setTimeout(() =>{
+   	selectedChoice.parentElement.classList.remove(classToApply);
+	getNewQuestion();
+}, 1000);
+   });
+
+});
+
+incrementScore =num =>{
+	score+=num;
+	scoreText.innerText =score;
+};
+
